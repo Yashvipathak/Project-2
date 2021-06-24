f00c0d5d8cf2fcd55e30588e8373afd1a2668470
Yashvipathak <82033896+Yashvipathak@users.noreply.github.com>
2021-06-24 13:18:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("My first console project");
+var First_name = "Yashvi";
+var Second_name = "Pathak";
+console.log(First_name);
+console.log(Second_name);
+console.log(First_name.concat(Second_name));
+var a = "5"
+var b = "7"
+var add = a + b;
+console.log(add);
